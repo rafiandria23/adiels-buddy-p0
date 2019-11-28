194480393a3caa6a570a1f0ab37f62e3da85c60d
Adam Rafiandri <rafiandria23@gmail.com>
2019-11-28 14:26:40 +0700

    Found out how to make the left diagonal loop.

--- diagonal.js
+++ diagonal.js
@@ -5,14 +5,31 @@
     [d, e, f],
     [g, h, i]
   ]
-  -> diagonal kiri nya adalah "a", "e", "i"
-  -> diagonal kanan nya adalah "c", "e", "g"
+  -> diagonal kanan nya adalah "a", "e", "i"
+  -> diagonal kiri nya adalah "c", "e", "g"
   maka output dari function ini adalah array multidimensi,
   yang menampung diagonal kiri dan kanan nya
   output: [[ 'a', 'e', 'i' ], [ 'c', 'e', 'g' ] ]
 */
 function main (board) {
   // your code here
+
+  let result = [];
+  let tempLeft = [];
+  let tempRight = [];
+  
+  for (let i = 0; i < board.length; i++) {
+    tempLeft.push(board[i][board[i].length-1-i]);
+  }
+
+  for (let j = 0; j < board.length; j++) {
+    tempRight.push(board[j][j]);
+  }
+
+  result.push(tempLeft);
+  result.push(tempRight);
+
+  return result;
 }
 
 var board1 = [
